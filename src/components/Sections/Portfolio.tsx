a96953a33acfbec6cf50b2972eac70f5e03ec5d0
james1107443 <jc1107443@gmail.com>
2025-06-03 03:33:33 -0400

    edit

--- src/components/Sections/Portfolio.tsx
+++ src/components/Sections/Portfolio.tsx
@@ -12,18 +12,18 @@ import Section from '../Layout/Section';
 const Portfolio: FC = memo(() => {
   return (
     <Section className="bg-neutral-800" sectionId={SectionId.Portfolio}>
-      <div className="flex flex-col gap-y-8">
+      <div className="flex flex-col gap-y-2">
         <h2 className="self-center text-xl font-bold text-white">Check out some of my work</h2>
-        <div className=" w-full columns-2 md:columns-3 lg:columns-4">
+        <div className="flex justify-centerw-full columns-4 md:columns-4 lg:columns-4">
           {portfolioItems.map((item, index) => {
             const {title, image} = item;
             return (
-              <div className="pb-6" key={`${title}-${index}`}>
+              <div className="m-4"  key={`${title}-${index}`}>
                 <div
                   className={classNames(
                     'relative h-max w-full overflow-hidden rounded-lg shadow-lg shadow-black/30 lg:shadow-xl',
                   )}>
-                  <Image alt={title} className="h-full w-full" placeholder="blur" src={image} />
+                  <Image alt={title} className="h-full w-full flex justify-between" placeholder="blur" src={image} style={{width:"600px",height:"220px"}} />
                   <ItemOverlay item={item} />
                 </div>
               </div>
--- src/data/dataDef.ts
+++ src/data/dataDef.ts
@@ -1,7 +1,7 @@
-import {StaticImageData} from 'next/image';
-import {FC, ForwardRefExoticComponent, SVGProps} from 'react';
+import { StaticImageData } from 'next/image';
+import { FC, ForwardRefExoticComponent, SVGProps } from 'react';
 
-import {IconProps} from '../components/Icon/Icon';
+import { IconProps } from '../components/Icon/Icon';
 
 export interface HomepageMeta {
   title: string;
@@ -121,10 +121,8 @@ export const ContactType = {
   Phone: 'Phone',
   Location: 'Location',
   Github: 'Github',
-  LinkedIn: 'LinkedIn',
   Facebook: 'Facebook',
   Twitter: 'Twitter',
-  Instagram: 'Instagram',
 } as const;
 
 export type ContactType = (typeof ContactType)[keyof typeof ContactType];
